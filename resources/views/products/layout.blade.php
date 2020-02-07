d29df2ca6385b96f9199035d663f7a34abf49ae0
sytuan <59157265+hapo-tuanns@users.noreply.github.com>
2020-02-07 14:25:23 +0700

    fix

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -4,12 +4,6 @@
     <title>Layout CRUD</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap4/css/bootstrap.css') }}">
 </head>
-<style>
-.form-group textarea{
-    height: 150px;
-}
-
-</style>
 <body>
   
 <div class="container">
